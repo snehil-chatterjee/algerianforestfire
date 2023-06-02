cc84ff4dca8b08adb392febf4e97379a59ac308e
snehil-chatterjee <snehilchatterjee7924@gmail.com>
2023-06-02 18:58:42 +0530

    first commit

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
-from flask import Flask
+import pickle
+from flask import Flask,request,jsonify,render_template
+import numpy as np
+import pandas as pd
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<h1>Hello, World!</h1>"
+scaler=pickle.load(open("/config/workspace/models/scaler.pkl","rb"))
+model=pickle.load(open("/config/workspace/models/ridge_model.pkl","rb"))
+
+
+@app.route('/',methods=["GET","POST"])
+def predict_datapoint():
+    if request.method=="POST":
+        Temperature=float(request.form.get("Temperature"))
+        FFMC=float(request.form.get("FFMC"))
+        DMC=float(request.form.get("DMC"))
+        ISI=float(request.form.get("ISI"))
+        RH=float(request.form.get("RH"))
+        Ws=float(request.form.get("Ws"))
+        Rain=float(request.form.get("Rain"))
+        Classes=float(request.form.get("Classes"))
+        Region=float(request.form.get("Region"))
+        value=scaler.transform([[Temperature,FFMC,DMC,ISI,RH,Ws,Rain,Classes,Region]])
+        prediction=model.predict(value)
+        return render_template("Home_Page.html",result=prediction[0])
+    else:
+        return render_template("Home_Page.html")
 
 if __name__=="__main__":
     app.run(host="0.0.0.0")
